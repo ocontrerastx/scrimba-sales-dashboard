068ae397ba503ade6ea288aef7586f2751edda5c
Osvaldo Contreras <ocontreras.tx@gmail.com>
2025-11-20 15:36:34 -0600

    Bootstrapping project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="dashboard-wrapper">
+      <div className="chart-container">
+        <h2>Total Sales This Quarter ($)</h2>
+      </div>
+    </div>
+  );
+}
